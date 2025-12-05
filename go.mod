5cdcdf09fc3c2bf1b327c64e6102480cc77d519f
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-12-05 13:10:39 +0300

    feat: password hasher

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.22.2
 require github.com/rs/zerolog v1.34.0
 
 require (
+	github.com/alexedwards/argon2id v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
---
+++ pkg/hasher/hasher.go
@@ -0,0 +1,23 @@
+package hasher
+
+import "github.com/alexedwards/argon2id"
+
+var params = argon2id.DefaultParams
+
+func HashPassword(password string) (string, error) {
+	hash, err := argon2id.CreateHash(password, params)
+	if err != nil {
+		return "", err
+	}
+
+	return hash, nil
+}
+
+func ChechPassword(password string, hash string) (bool, error) {
+	match, err := argon2id.ComparePasswordAndHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+
+	return match, nil
+}
