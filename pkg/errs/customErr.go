5e3da503528289b95a7b0edb53fc9efcedd7cac8
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-12-05 17:42:16 +0300

    added errors

--- pkg/errs/customErr.go
+++ pkg/errs/customErr.go
@@ -7,4 +7,5 @@ var (
 	ErrUserUsernameExists = errors.New("username already exists")
 	ErrUserNotFound       = errors.New("user not found")
 	ErrGameNotFound       = errors.New("game not found")
+	ErrUserPassword       = errors.New("invalid password and/or login")
 )
