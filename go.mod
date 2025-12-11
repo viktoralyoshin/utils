f36472b14e084d96bfd797fed65538259f14d655
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-12-11 16:01:53 +0300

    feat: grpc errors mapper

--- go.mod
+++ go.mod
@@ -1,16 +1,19 @@
 module github.com/viktoralyoshin/utils
 
-go 1.22.2
+go 1.24.0
 
 require (
 	github.com/alexedwards/argon2id v1.0.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/rs/zerolog v1.34.0
+	google.golang.org/grpc v1.77.0
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
---
+++ pkg/errs/grpcerr.go
@@ -0,0 +1,53 @@
+package errs
+
+import (
+	"net/http"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// HTTPStatus возвращает соответствующий HTTP код и чистое сообщение ошибки
+// на основе gRPC ошибки.
+// Возвращает: (httpCode, errorMessage)
+func HTTPStatus(err error) (int, string) {
+	if err == nil {
+		return http.StatusOK, ""
+	}
+
+	st, ok := status.FromError(err)
+	if !ok {
+		// Если ошибка не от gRPC (например, ошибка маршалинга или баз данных, не обернутая в status)
+		// Возвращаем 500 и стандартный текст, чтобы не светить кишки наружу
+		return http.StatusInternalServerError, "internal server error"
+	}
+
+	switch st.Code() {
+	case codes.OK:
+		return http.StatusOK, ""
+	case codes.NotFound:
+		return http.StatusNotFound, st.Message()
+	case codes.InvalidArgument:
+		return http.StatusBadRequest, st.Message()
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized, st.Message()
+	case codes.PermissionDenied:
+		return http.StatusForbidden, st.Message()
+	case codes.AlreadyExists:
+		return http.StatusConflict, st.Message()
+	case codes.Internal:
+		return http.StatusInternalServerError, "internal server error"
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable, st.Message()
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout, st.Message()
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests, st.Message()
+	case codes.Aborted:
+		return http.StatusConflict, st.Message()
+	case codes.Unimplemented:
+		return http.StatusNotImplemented, st.Message()
+	default:
+		return http.StatusInternalServerError, "internal server error"
+	}
+}
