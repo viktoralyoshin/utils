2caa1afce27aab51a51b5c5540a9082918af01d5
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-12-11 16:11:44 +0300

    fix: logger environment

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -8,9 +8,22 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func Setup() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stderr,
-		TimeFormat: time.RFC3339,
-	})
+func Setup(env string) {
+	if env == "prod" {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	if env == "local" || env == "dev" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: time.RFC3339,
+			NoColor:    false,
+		})
+	} else {
+		log.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	}
 }
